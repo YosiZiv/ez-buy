200586c5ddf54e9f33b985628d905c36ebe234c0
marchipot <jrfighter39@gmail.com>
2020-06-30 01:21:37 +0300

    commit

--- client/pages/auth/register.jsx
+++ client/pages/auth/register.jsx
@@ -1,5 +1,10 @@
+// @ts-nocheck
+import { useState } from 'react';
+
 import Head from 'next/head';
 
+import axios from 'axios';
+import { useForm } from "react-hook-form";
 
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -8,12 +13,13 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 
+import FileUpload from '../../components/FileUpload';
+import useRequest from '../../hooks/useRequest';
 
 const useStyles = makeStyles( ( theme ) => ( {
     paper: {
@@ -37,7 +43,19 @@ const useStyles = makeStyles( ( theme ) => ( {
 
 export default function Register()
 {
+
+    const { doRequest, requestErrors } = useRequest( {
+        url: '/api/users/singup', method: 'post', body: { data }, onSuccess: ( response ) => console.log( response )
+    } );
+    const [imgUrl, setImgUrl] = useState( "" );
     const classes = useStyles();
+    const { handleSubmit, register, errors } = useForm();
+
+    const onSubmit = values =>
+    {
+        console.log( values );
+        doRequest();
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -50,7 +68,7 @@ export default function Register()
                 <Typography component="h1" variant="h5">
                     הירשם
         </Typography>
-                <form className={classes.form} noValidate>
+                <form onSubmit={handleSubmit( onSubmit )} className={classes.form} noValidate>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                             <TextField
@@ -61,7 +79,19 @@ export default function Register()
                                 id="firstName"
                                 label="שם פרטי"
                                 autoFocus
+                                inputRef={register( {
+                                    required: 'שם פרטי שדה חובה',
+                                    minLength: {
+                                        value: 2,
+                                        message: 'שם משפחה חייב להכיל לפחות 2 תווים'
+                                    }
+                                } )}
+
                             />
+                            <span className="errorMessage">
+                                {errors.firstName && errors.firstName.message}
+                            </span>
+
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <TextField
@@ -71,7 +101,17 @@ export default function Register()
                                 label="שם משפחה"
                                 name="lastName"
                                 autoComplete="lname"
+                                inputRef={register( {
+                                    required: 'שם משפחה הוא שדה חובה',
+                                    minLength: {
+                                        value: 2,
+                                        message: 'שם משפחה חייב להכיל לפחות 2 תווים'
+                                    }
+                                } )}
                             />
+                            <span className="errorMessage">
+                                {errors.lastName && errors.lastName.message}
+                            </span>
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
@@ -81,7 +121,21 @@ export default function Register()
                                 label="כתובת מייל"
                                 name="email"
                                 autoComplete="email"
+                                inputRef={register( {
+                                    required: 'דואר אולקטורני הוא שדה חובה',
+                                    minLength: {
+                                        value: 2,
+                                        message: 'דואר אלקטרוני חייב להכיל לפחות 2 תווים',
+                                    },
+                                    pattern: {
+                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: 'כתובת דואר אלקטורני לא תקינה'
+                                    }
+                                } )}
                             />
+                            <span className="errorMessage">
+                                {errors.email && errors.email.message}
+                            </span>
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
@@ -91,7 +145,17 @@ export default function Register()
                                 label="מספר טלפון"
                                 name="phone"
                                 autoComplete="phone"
+                                inputRef={register( {
+                                    required: 'טלפון הוא שדה חובה',
+                                    minLength: {
+                                        value: 9,
+                                        message: 'טלפון חייב להכיל לפחות 9 ספרות'
+                                    }
+                                } )}
                             />
+                            <span className="errorMessage">
+                                {errors.phone && errors.phone.message}
+                            </span>
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
@@ -103,12 +167,43 @@ export default function Register()
                                 type="password"
                                 id="password"
                                 autoComplete="current-password"
+                                inputRef={register( {
+                                    required: 'סיסמא הוא שדה חובה',
+                                    minLength: {
+                                        value: 6,
+                                        message: 'סיסמא חייבת להכיל לפחות 6 תווים'
+                                    }
+                                } )}
                             />
+                            <span className="errorMessage">
+                                {errors.password && errors.password.message}
+                            </span>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FileUpload
+                                id="companyLogo"
+                                name="Company Logo"
+                                required
+                                defaultValue="userAvatar"
+                                uploadComplete={( { id, value } ) =>
+                                {
+
+                                    console.log( id, value );
+                                    setImgUrl( value );
+                                }}
+                                text="תמונת פרופיל"
+                            />
+                            {imgUrl !== '' && <div>
+                                <img style={{ width: '100px', height: '100px' }} src={imgUrl} />
+                            </div>}
+                            <input name="imgUrl" ref={register( {} )} style={{ display: 'none' }} type='text' value={imgUrl} onChange={() => { }} />
                         </Grid>
                         <Grid item xs={12}>
                             <FormControlLabel
+                                name="imgUrl"
+                                inputRef={register( {} )}
                                 control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="אני מעוניין לקבל תוכן עידכונים ותוכן שיווקי לכתובת מייל הזו"
+                                label="אני מעוניין לקבל  עידכונים ותוכן שיווקי לכתובת מייל הזו"
                             />
                         </Grid>
                     </Grid>
