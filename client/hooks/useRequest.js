8b42c6b4a60d1f92ca13a93cd43c02e020d2dadd
marchipot <jrfighter39@gmail.com>
2020-06-30 02:43:51 +0300

    login useRequest

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default ({ url, method, onSuccess }) => {
-  const [requestErrors, setRequestErrors] = useState(null);
-  const doRequest = async (body) => {
-    try {
-      requestErrors(null);
-      const response = await axios[method](url, body);
-      if (onSuccess) {
-        onSuccess(response.data);
+export default ( { url, method, onSuccess } ) =>
+{
+  const [requestErrors, setRequestErrors] = useState( null );
+  const doRequest = async ( body ) =>
+  {
+    try
+    {
+      setRequestErrors( null );
+      const response = await axios[method]( url, body );
+      if ( onSuccess )
+      {
+        onSuccess( response.data );
       }
       return response.data;
-    } catch (err) {
-      requestErrors(
+    } catch ( err )
+    {
+      setRequestErrors(
         <div className='alert alert-danger'>
           <h4>Ooops....</h4>
-          <ul className='my-0'>
-            {err.response.data.errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
         </div>
       );
     }
--- client/pages/auth/register.jsx
+++ client/pages/auth/register.jsx
@@ -1,11 +1,9 @@
 // @ts-nocheck
 import { useState } from "react";
-
+import Router from 'next/router';
 import Head from "next/head";
-
 import axios from "axios";
 import { useForm } from "react-hook-form";
-
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -20,39 +18,45 @@ import Container from "@material-ui/core/Container";
 import FileUpload from "../../components/FileUpload";
 import useRequest from "../../hooks/useRequest";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles( ( theme ) => ( {
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing( 8 ),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
   avatar: {
-    margin: theme.spacing(1),
+    margin: theme.spacing( 1 ),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing( 3 ),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing( 3, 0, 2 ),
   },
-}));
+} ) );
 
-export default function Register() {
-  const { doRequest, requestErrors } = useRequest({
+export default function Register()
+{
+  const { doRequest, requestErrors } = useRequest( {
     url: "/api/users/signup",
     method: "post",
-    onSuccess: (response) => console.log(response),
-  });
-  const [imgUrl, setImgUrl] = useState("");
+    onSuccess: ( response ) =>
+    {
+      console.log( response );
+      Router.push( '/auth/login' );
+    },
+  } );
+  const [imgUrl, setImgUrl] = useState( "" );
   const classes = useStyles();
   const { handleSubmit, register, errors } = useForm();
 
-  const onSubmit = (values) => {
-    console.log(values);
-    doRequest(values);
+  const onSubmit = ( values ) =>
+  {
+    console.log( values );
+    doRequest( values );
   };
 
   return (
@@ -66,7 +70,7 @@ export default function Register() {
           הירשם
         </Typography>
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit( onSubmit )}
           className={classes.form}
           noValidate
         >
@@ -80,13 +84,13 @@ export default function Register() {
                 id='firstName'
                 label='שם פרטי'
                 autoFocus
-                inputRef={register({
+                inputRef={register( {
                   required: "שם פרטי שדה חובה",
                   minLength: {
                     value: 2,
                     message: "שם משפחה חייב להכיל לפחות 2 תווים",
                   },
-                })}
+                } )}
               />
               <span className='errorMessage'>
                 {errors.firstName && errors.firstName.message}
@@ -100,13 +104,13 @@ export default function Register() {
                 label='שם משפחה'
                 name='lastName'
                 autoComplete='lname'
-                inputRef={register({
+                inputRef={register( {
                   required: "שם משפחה הוא שדה חובה",
                   minLength: {
                     value: 2,
                     message: "שם משפחה חייב להכיל לפחות 2 תווים",
                   },
-                })}
+                } )}
               />
               <span className='errorMessage'>
                 {errors.lastName && errors.lastName.message}
@@ -120,7 +124,7 @@ export default function Register() {
                 label='כתובת מייל'
                 name='email'
                 autoComplete='email'
-                inputRef={register({
+                inputRef={register( {
                   required: "דואר אולקטורני הוא שדה חובה",
                   minLength: {
                     value: 2,
@@ -130,10 +134,26 @@ export default function Register() {
                     value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                     message: "כתובת דואר אלקטורני לא תקינה",
                   },
-                })}
+                } )}
               />
               <span className='errorMessage'>
                 {errors.email && errors.email.message}
+              </span>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                fullWidth
+                id='address'
+                label='כתובת '
+                name='address'
+                autoComplete='address'
+                inputRef={register( {
+                  required: "כתובת היא שדה חובה",
+                } )}
+              />
+              <span className='errorMessage'>
+                {errors.address && errors.address.message}
               </span>
             </Grid>
             <Grid item xs={12}>
@@ -144,13 +164,13 @@ export default function Register() {
                 label='מספר טלפון'
                 name='phone'
                 autoComplete='phone'
-                inputRef={register({
+                inputRef={register( {
                   required: "טלפון הוא שדה חובה",
                   minLength: {
                     value: 9,
                     message: "טלפון חייב להכיל לפחות 9 ספרות",
                   },
-                })}
+                } )}
               />
               <span className='errorMessage'>
                 {errors.phone && errors.phone.message}
@@ -166,13 +186,13 @@ export default function Register() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
-                inputRef={register({
+                inputRef={register( {
                   required: "סיסמא הוא שדה חובה",
                   minLength: {
                     value: 6,
                     message: "סיסמא חייבת להכיל לפחות 6 תווים",
                   },
-                })}
+                } )}
               />
               <span className='errorMessage'>
                 {errors.password && errors.password.message}
@@ -184,9 +204,10 @@ export default function Register() {
                 name='Company Logo'
                 required
                 defaultValue='userAvatar'
-                uploadComplete={({ id, value }) => {
-                  console.log(id, value);
-                  setImgUrl(value);
+                uploadComplete={( { id, value } ) =>
+                {
+                  console.log( id, value );
+                  setImgUrl( value );
                 }}
                 text='תמונת פרופיל'
               />
@@ -200,17 +221,15 @@ export default function Register() {
               )}
               <input
                 name='imgUrl'
-                ref={register({})}
+                ref={register( {} )}
                 style={{ display: "none" }}
                 type='text'
                 value={imgUrl}
-                onChange={() => {}}
+                onChange={() => { }}
               />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
-                name='imgUrl'
-                inputRef={register({})}
                 control={<Checkbox value='allowExtraEmails' color='primary' />}
                 label='אני מעוניין לקבל  עידכונים ותוכן שיווקי לכתובת מייל הזו'
               />
